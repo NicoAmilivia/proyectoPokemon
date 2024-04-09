46403348467707f97d92fb2d15f7eb299d0ae5dd
FernandoCorbalan <fernandosancor@gmail.com>
2024-04-09 13:04:23 +0200

    Menu principal

--- src/main/java/es/cesur/progprojectpok/HelloApplication.java
+++ src/main/java/es/cesur/progprojectpok/HelloApplication.java
@@ -22,8 +22,8 @@ public class HelloApplication extends Application {
 
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 728, 408);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
