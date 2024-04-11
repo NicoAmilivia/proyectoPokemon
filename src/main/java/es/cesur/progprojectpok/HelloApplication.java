c0a38e04cb2ed212eed71d6789fd1aacfada4806
FernandoCorbalan <fernandosancor@gmail.com>
2024-04-11 12:52:01 +0200

    Cambio menu principal

--- src/main/java/es/cesur/progprojectpok/HelloApplication.java
+++ src/main/java/es/cesur/progprojectpok/HelloApplication.java
@@ -22,7 +22,7 @@ public class HelloApplication extends Application {
 
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("Login");
         stage.setScene(scene);
