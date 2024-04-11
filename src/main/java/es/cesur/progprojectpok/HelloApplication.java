50a5bb01b5b7d5b171aee5c9e6760d1dffc9b645
nico <nicoamilivia1917@gmail.com>
2024-04-11 12:38:58 +0200

    Cambios en la pantalla de login.

--- src/main/java/es/cesur/progprojectpok/HelloApplication.java
+++ src/main/java/es/cesur/progprojectpok/HelloApplication.java
@@ -22,7 +22,7 @@ public class HelloApplication extends Application {
 
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/centro-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("Login");
         stage.setScene(scene);
