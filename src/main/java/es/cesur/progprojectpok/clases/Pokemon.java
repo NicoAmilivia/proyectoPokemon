d8b490182c09a7dab98795d240c6d34e6d37a784
FernandoCorbalan <fernandosancor@gmail.com>
2024-04-19 14:25:52 +0200

    Clase pokemon + Unique

---
+++ src/main/java/es/cesur/progprojectpok/clases/Pokemon.java
@@ -0,0 +1,61 @@
+/*package es.cesur.progprojectpok.clases;
+
+public class Pokemon {
+
+    //ATRIBUTOS
+    private String nombre;
+    private int vitalidad;
+    private int ataque;
+
+    private int defensa;
+    private int nivel;
+    private int fertilidad;
+    private Estado estado;
+    private Sexo sexo;
+    private String rutaImagen;
+    private String rutaSonidoCombate;
+    public static final int TOTAL_MOVIMIENTOS=4;
+    public static final int FACTOR_SUBIDA_NVL=1;
+    private int ataqueEspecial;
+    private int defensaEspecial;
+    private static final int VALOR_INICIAL_FERT =5;
+    public static final int CRIANZA_ATTR_MIN_ALEATORIO =1;
+    private static final int CRIANZA_ATTR_MAX_ALEATORIO =10;
+
+
+    //METODOS
+
+    public void subirNivel(int experienciaGanada){
+
+    }
+
+
+    public void atacarPokemon(Movimiento movimientoAtaque){
+
+    }
+
+    public void comprobarVentajaRival(Pokemon pokemonRival){
+
+
+    }
+
+    public void aprenderMovimiento(Movimiento movActualASustituir,Movimiento movNuevoPosible){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
