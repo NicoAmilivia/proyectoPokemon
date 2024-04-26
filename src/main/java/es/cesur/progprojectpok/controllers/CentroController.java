8454a3a6c4ff1160b13e9be28a195d8f4d9b3c2b
nico <nicoamilivia1917@gmail.com>
2024-04-26 09:35:50 +0200

    Solucion de error en CentroController.

--- src/main/java/es/cesur/progprojectpok/controllers/CentroController.java
+++ src/main/java/es/cesur/progprojectpok/controllers/CentroController.java
@@ -8,19 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javafx.scene.image.ImageView;
-
-
-
-
-
 
 public class CentroController {
 
@@ -84,16 +80,10 @@ public class CentroController {
     @FXML
     private Text mote6;
 
-
-
-
-
     private boolean mensajeMostrado = false;
 
-
     public void initialize() {
-        try {
-            Connection connection = DBConnection.getConnection();
+        try (Connection connection = DBConnection.getConnection()) {
             String sql = "SELECT pd.IMAGEN, p.VIDA_ACTUAL, p.VITALIDAD, p.MOTE " +
                     "FROM POKEMON p " +
                     "INNER JOIN POKEDEX pd ON p.NUM_POKEDEX = pd.NUM_POKEDEX " +
@@ -104,67 +94,83 @@ public class CentroController {
 
             int i = 1;
 
-            while (resultSet.next()) {
+            while (resultSet.next() && i <= 6) {
                 String imageUrl = resultSet.getString("IMAGEN");
                 int vidaActual = resultSet.getInt("VIDA_ACTUAL");
                 int vidaMaxima = resultSet.getInt("VITALIDAD");
                 String motePokemon = resultSet.getString("MOTE");
                 Image image = new Image(getClass().getResource(imageUrl).toExternalForm());
 
-                // Establecer la imagen del Pokémon
-                switch (i) {
-                    case 1:
-                        pokemon1.setImage(image);
-                        progressPK1.setProgress((double) vidaActual / vidaMaxima);
-                        mote1.setText(motePokemon);
-                        break;
-                    case 2:
-                        pokemon2.setImage(image);
-                        progressPK2.setProgress((double) vidaActual / vidaMaxima);
-                        mote2.setText(motePokemon);
-                        break;
-                    case 3:
-                        pokemon3.setImage(image);
-                        progressPK3.setProgress((double) vidaActual / vidaMaxima);
-                        mote3.setText(motePokemon);
-                        break;
-                    case 4:
-                        pokemon4.setImage(image);
-                        progressPK4.setProgress((double) vidaActual / vidaMaxima);
-                        mote4.setText(motePokemon);
-                        break;
-                    case 5:
-                        pokemon5.setImage(image);
-                        progressPK5.setProgress((double) vidaActual / vidaMaxima);
-                        mote5.setText(motePokemon);
-                        break;
-                    case 6:
-                        pokemon6.setImage(image);
-                        progressPK6.setProgress((double) vidaActual / vidaMaxima);
-                        mote6.setText(motePokemon);
-                        break;
+                ImageView pokemonImageView = switch (i) {
+                    case 1 -> pokemon1;
+                    case 2 -> pokemon2;
+                    case 3 -> pokemon3;
+                    case 4 -> pokemon4;
+                    case 5 -> pokemon5;
+                    case 6 -> pokemon6;
+                    default -> null;
+                };
+
+                ProgressBar pokemonProgressBar = switch (i) {
+                    case 1 -> progressPK1;
+                    case 2 -> progressPK2;
+                    case 3 -> progressPK3;
+                    case 4 -> progressPK4;
+                    case 5 -> progressPK5;
+                    case 6 -> progressPK6;
+                    default -> null;
+                };
+
+                Text moteText = switch (i) {
+                    case 1 -> mote1;
+                    case 2 -> mote2;
+                    case 3 -> mote3;
+                    case 4 -> mote4;
+                    case 5 -> mote5;
+                    case 6 -> mote6;
+                    default -> null;
+                };
+
+                if (pokemonImageView != null && pokemonProgressBar != null && moteText != null) {
+                    pokemonImageView.setImage(image);
+                    pokemonProgressBar.setProgress((double) vidaActual / vidaMaxima);
+                    moteText.setText(motePokemon);
                 }
 
                 i++;
             }
-
-            connection.close(); // Cerrar la conexión
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    @FXML
+    public void recuperarOnAction() {
+        try (Connection connection = DBConnection.getConnection()) {
+            String sql = "UPDATE POKEMON SET VIDA_ACTUAL = VITALIDAD WHERE CAJA = 0";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            int rowsAffected = statement.executeUpdate();
 
+            if (rowsAffected > 0) {
+                progressPK1.setProgress(1.0);
+                progressPK2.setProgress(1.0);
+                progressPK3.setProgress(1.0);
+                progressPK4.setProgress(1.0);
+                progressPK5.setProgress(1.0);
+                progressPK6.setProgress(1.0);
 
-
-
-
-
+                if (!mensajeMostrado) {
+                    logCentro.appendText("¡Pokémons recuperados! ✔");
+                    mensajeMostrado = true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
-
     private void irMenuFromCentroOnAction() {
-
         Stage stage = (Stage) irMenuFromCentroButton.getScene().getWindow();
         stage.close();
 
@@ -176,152 +182,7 @@ public class CentroController {
             menuStage.setScene(scene);
             menuStage.show();
         } catch (IOException e) {
-            e.printStackTrace(); // Maneja el error apropiadamente
-        }
-    }
-
-
-
-
-    @FXML
-    public void recuperarOnAction() {
-        try {
-            // Obtener la conexión a la base de datos
-            Connection connection = DBConnection.getConnection();
-
-            // Consulta SQL para obtener la vida actual de cada Pokémon
-            String sql = "SELECT ID_POKEMON, VIDA_ACTUAL FROM POKEMON WHERE CAJA = 0";
-
-            // Preparar la declaración SQL
-            PreparedStatement statement = connection.prepareStatement(sql);
-
-            // Ejecutar la consulta
-            ResultSet resultSet = statement.executeQuery();
-
-            // Actualizar la vida de cada Pokémon
-            while (resultSet.next()) {
-                int id_pokemon = resultSet.getInt("ID_POKEMON");
-                int vidaActual = resultSet.getInt("VIDA_ACTUAL");
-
-                // Actualizar la vida del Pokémon en la base de datos
-                actualizarVidaActual(id_pokemon);
-            }
-
-            // Cerrar la conexión y liberar recursos
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        try{
-
-            Connection connection = DBConnection.getConnection();
-            String sql = "SELECT pd.IMAGEN " +
-                    "FROM POKEMON p " +
-                    "INNER JOIN POKEDEX pd ON p.NUM_POKEDEX = pd.NUM_POKEDEX " +
-                    "WHERE p.CAJA = 0;";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet resultSet = statement.executeQuery();
-
-
-
-
-
-            int i = 1;
-
-            while (resultSet.next()) {
-
-
-                if (i == 1) {
-                   progressPK1.setProgress(1.0);
-                } else if (i == 2) {
-                    progressPK2.setProgress(1.0);
-                } else if (i == 3) {
-                    progressPK3.setProgress(1.0);
-                } else if (i == 4) {
-                    progressPK4.setProgress(1.0);
-                } else if (i == 5) {
-                    progressPK5.setProgress(1.0);
-                } else if (i == 6) {
-                    progressPK6.setProgress(1.0);
-                }
-
-
-
-                i++;
-
-
-            }
-
-
-        } catch(SQLException e){
-
-            e.printStackTrace();
-
-        }
-
-
-
-
-        if (!mensajeMostrado) {
-            // Mostrar el mensaje solo si no se ha mostrado antes
-            logCentro.appendText("¡Pokémons recuperados! ✔");
-            mensajeMostrado = true; // Marcar el mensaje como mostrado
-        }
-
-
-
-
-
-
-    }
-
-    // Método para actualizar la vida actual de un Pokémon en la base de datos
-    private void actualizarVidaActual(int id_Pokemon) {
-        try {
-            // Obtener la conexión a la base de datos
-            Connection connection = DBConnection.getConnection();
-
-            // Consulta SQL para obtener la vitalidad del Pokémon
-            String sqlSelect = "SELECT VITALIDAD FROM POKEMON WHERE CAJA = 0 AND ID_POKEMON = ?";
-            PreparedStatement statementSelect = connection.prepareStatement(sqlSelect);
-            statementSelect.setInt(1, id_Pokemon);
-            ResultSet resultSet = statementSelect.executeQuery();
-
-            int vidaMaxima = 0;
-            if (resultSet.next()) {
-                vidaMaxima = resultSet.getInt("VITALIDAD");
-            }
-
-            // Consulta SQL para actualizar la vida actual del Pokémon
-            String sqlUpdate = "UPDATE POKEMON SET VIDA_ACTUAL = ? WHERE ID_POKEMON = ?";
-            PreparedStatement statementUpdate = connection.prepareStatement(sqlUpdate);
-            statementUpdate.setInt(1, vidaMaxima);
-            statementUpdate.setInt(2, id_Pokemon);
-
-            // Ejecutar la actualización
-            int rowsAffected = statementUpdate.executeUpdate();
-
-            if (rowsAffected > 0) {
-                System.out.println("La vida del Pokémon número " + id_Pokemon + " ha sido restablecida al máximo.");
-            } else {
-                System.out.println("No se pudo restablecer la vida del Pokémon número " + id_Pokemon + ".");
-            }
-
-            // Cerrar la conexión y liberar recursos
-            connection.close();
-        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
-
-
-
-
-
-
-
-
 }
